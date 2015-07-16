8c3ce27e615e5afa80c7161e5722ebff46d1d28a
jakciehoo <niat05ethjh1112@163.com>
2015-07-16 17:58:42 +0800

    Initial Commit

---
+++ MomentsPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MomentsPhoto
+//
+//  Created by HooJackie on 15/7/16.
+//  Copyright (c) 2015年 jackie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MomentsPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MomentsPhoto
+//
+//  Created by HooJackie on 15/7/16.
+//  Copyright (c) 2015年 jackie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
